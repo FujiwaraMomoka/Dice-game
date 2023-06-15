295cf6687ecdf3c4ad32dcc872516c15bb9eed73
FujiwaraMomoka <s1300166@u-aizu.ac.jp>
2023-06-16 00:06:14 +0900

    Won/lost message modified

--- comment.java
+++ comment.java
@@ -1,10 +1,21 @@
 import java.util.Scanner;
+import java.util.Random;
 
 class comment{
     public static void main(String[] args){
     Scanner scanner = new Scanner(System.in);
     System.out.println("What is your name?");
     String name = scanner.next();
-    System.out.println("Hello, " + name);
+    System.out.println("Hello, " + name + "!");
+    System.out.println("Rolling dice...");
+    Random rand = new Random();
+    int one = rand.nextInt(6) + 1;
+    int two = rand.nextInt(6) + 1;
+    System.out.println("Die 1: " + one);
+    System.out.println("Die 2: " + two);
+    int total = one + two;
+    System.out.println("Total value: " + total);
+    if(total>7) System.out.println(name +" won!");
+    else System.out.println(name +" lost");
     }
 }
